397339e0e5142b3a06244d4521009d2aa906395a
raed-0019 <138981715+raed-0019@users.noreply.github.com>
2026-09-05 15:29:44 +0300

    Correct age input and enhance age-related output
    Fix input errors and add future age calculation.

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 name = input("what is your name? ")
-age = input("How old are you? ")
+age = int (intput("How old are you? "))
 print("Hello",name )
-preint("You are",age , "years old")
+print("You are",age , "years old")
+future_age = age + 5 
+print("After 5 years, you. will be ",future_age,
+      if age>18:
+        print("You are 18 or older")
+      else:
+        print("You are under 18")
